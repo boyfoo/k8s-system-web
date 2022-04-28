0ab442a3d801449de3775a699486794e5ccb3bd1
zhengxin <zx358317301@qq.com>
2022-04-28 17:19:23 +0800

    feat

--- src/views/nodes/nodelist.vue
+++ src/views/nodes/nodelist.vue
@@ -16,39 +16,56 @@
       <el-table-column label="名称" width="350">
         <template slot-scope="scope">
           <p>{{ scope.row.Name }} <br/>
-            <span style="color: gray">{{scope.row.IP}}/{{scope.row.HostName}}</span>
+          <span style="color: gray">{{scope.row.IP}}/{{scope.row.HostName}}</span>
             <br/>
             <el-tag type="success" v-for="label in scope.row.Lables">{{label}}</el-tag>
+            <br/>
+            <el-tag type="danger" v-for="label in scope.row.Taints">{{label}}</el-tag>
           </p>
         </template>
       </el-table-column>
-
-      <el-table-column label="创建时间" width="170" align="center">
+      <el-table-column label="CPU" width="100" align="center">
+        <template slot-scope="scope">
+          {{ scope.row.Capacity.Cpu }}核
+        </template>
+      </el-table-column>
+      <el-table-column label="内存" width="100" align="center">
+        <template slot-scope="scope">
+          {{ Math.round(scope.row.Capacity.Memory/1000/1000/1000) }}G
+        </template>
+      </el-table-column>
+      <el-table-column label="Pods" width="100" align="center">
+        <template slot-scope="scope">
+          {{ scope.row.Usage.Pods }}/{{ scope.row.Capacity.Pods }}
+        </template>
+      </el-table-column>
+      <el-table-column label="创建时间" width="100" align="center">
         <template slot-scope="scope">
           {{ scope.row.CreateTime }}
         </template>
       </el-table-column>
+
       <el-table-column label="操作" width="100" align="center">
         <template slot-scope="scope">
-        </template>
+       </template>
       </el-table-column>
     </el-table>
   </div>
 </template>
 <script>
-import { getList } from '@/api/nodes'
-import { NewClient } from "@/utils/ws";
-export default {
-  data(){
-    return {
-      list: null,
-      listLoading: true,
-      wsClient:null
-    }
-  },
-  created() {
-    this.fetchData()
-  },
+  import { getList } from '@/api/nodes'
+  import { NewClient } from "@/utils/ws";
+  export default {
+    data(){
+      return {
+        list: null,
+        listLoading: true,
+        wsClient:null
+      }
+    },
+    created() {
+      this.fetchData()
+    },
   methods: {
     fetchData()
     {
@@ -71,7 +88,7 @@ export default {
       }
 
     }
-    ,
+  ,
   }
-}
+  }
 </script>
